a59548fbebb0aa7892fe16c56ba90dc12607b041
Kavindi Saubhagya <110480920+KSaubhagya@users.noreply.github.com>
2025-10-11 22:40:36 +0530

    Create  Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+         int len =A.length(); String rev="";
+
+        for(int i=len-1;i>=0;i--){
+            rev+=A.charAt(i);
+        }
+
+     if(rev.equals(A)){
+        System.out.println("Yes");
+    }else{
+        System.out.println("No");
+    }
+ sc.close(); 
+
+        
+    }
+}
+
+
+
