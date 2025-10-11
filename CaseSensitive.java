532112d36f8b25825a4eb2c17c13e1de91ea97b8
Kavindi Saubhagya <110480920+KSaubhagya@users.noreply.github.com>
2025-10-11 22:43:39 +0530

    Create CaseSensitive.java

---
+++ CaseSensitive.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+
+   public static boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) return false;
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        for (int i=0;i<a.length();i++){
+            b= b.replaceFirst(String.valueOf(a.charAt(i)), "");
+            
+        }
+        return (b.equals("")) ?  true :  false;
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
