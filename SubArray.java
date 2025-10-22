d2d4f97dd67c71140746af592545c41451cb9541
Kavindi Saubhagya <110480920+KSaubhagya@users.noreply.github.com>
2025-10-22 21:18:41 +0530

    Create SubArray.java

---
+++ SubArray.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+//Read array size
+int n = sc.nextInt();
+int[] arr = new int[n];
+
+//Read array element 
+for (int i = 0; i < n; i++) {
+    arr[i] = sc.nextInt();
+}
+
+int count = 0;
+
+for (int i = 0; i < n; i++) {
+    int sum = 0;
+
+    for (int j = i; j < n; j++) {
+        sum += arr[j];
+
+        if(sum < 0){
+            count++;
+        }
+    }
+}
+System.out.println(count);
+
+} }
