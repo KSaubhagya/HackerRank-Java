af120d46294fa8411dda49c6b7c9589d78131669
Kavindi Saubhagya <110480920+KSaubhagya@users.noreply.github.com>
2025-11-06 21:34:27 +0530

    Update EOF commentary

--- EOF.java
+++ EOF.java
@@ -7,7 +7,6 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* */
         Scanner sc= new Scanner (System.in);
         int i=1;
         while(sc.hasNext()){                //end of the file
