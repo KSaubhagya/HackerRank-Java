606b5c3086d960a059d5a60bf6780b55c9be4bb0
Kavindi Saubhagya <110480920+KSaubhagya@users.noreply.github.com>
2026-04-09 17:03:22 +0530

    Update calender

--- calender.java
+++ calender.java
@@ -8,9 +8,6 @@ import java.util.regex.*;
 
 class Result {
     /*
-     * Complete the 'findDay' function below.
-     * The function is expected to return a STRING.
-     * The function accepts following parameters:
      *  1. INTEGER month
      *  2. INTEGER day
      *  3. INTEGER year
