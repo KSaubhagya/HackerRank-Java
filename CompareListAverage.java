dc23a7df97d3735ac647f4ad1d96787e1a6230ca
Kavindi Saubhagya <110480920+KSaubhagya@users.noreply.github.com>
2026-05-19 21:25:27 +0530

    Update description

--- CompareListAverage.java
+++ CompareListAverage.java
@@ -33,8 +33,6 @@ class Result {
 
     }
 
-
-
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
