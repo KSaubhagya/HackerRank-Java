a1b686d737fab9a0fb57bb3165e767009305dcd1
Kavindi Saubhagya <110480920+KSaubhagya@users.noreply.github.com>
2026-05-19 21:25:12 +0530

    Update description

--- CompareListAverage.java
+++ CompareListAverage.java
@@ -10,7 +10,6 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-
 class Result {
 
     public static int countResponseTimeRegressions(List<Integer> responseTimes) {
