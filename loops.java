30e6171f306ca6f0822e7bb211951f8c6ee1ced7
Kavindi Saubhagya <110480920+KSaubhagya@users.noreply.github.com>
2026-06-07 23:41:03 +0530

    Update exceptions

--- loops.java
+++ loops.java
@@ -7,7 +7,7 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));  //this is instead of scanner class
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
