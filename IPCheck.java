cdfb7eaf5192380c0ec4c849cc5673f4c778e776
Kavindi Saubhagya <110480920+KSaubhagya@users.noreply.github.com>
2026-07-28 22:25:38 +0530

    Update IP check

--- IPCheck.java
+++ IPCheck.java
@@ -3,7 +3,6 @@ import java.util.regex.Pattern;
 import java.util.Scanner;
    
 class Solution{ 
-
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         while(in.hasNext()){
